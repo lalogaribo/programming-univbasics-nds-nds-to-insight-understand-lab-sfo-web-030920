8cd1d1abfc0ad97e17e7a666dc26f4d83b8cdea4
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-01-15 21:38:06 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,6 +12,6 @@ def print_first_directors_movie_titles
   nds = directors_database
   first_director = nds[0][:movies]
   first_director.each do |movie|
-    puts movie
+    puts movie[:title]
   end
 end
